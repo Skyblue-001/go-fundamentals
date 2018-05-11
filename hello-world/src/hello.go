184aa0d17245f2f7100bb06ff1dde52e74ac75a7
Teddy Ghim <teddyghim@gmail.com>
2018-05-11 04:58:12 +0200

    hello world

---
+++ hello-world/src/hello.go
@@ -0,0 +1,15 @@
+package main
+
+var f float32 = 11.
+var i, j, k int = 1, 2, 3
+
+const (
+    Apple = iota // 0
+    Grape        // 1
+    Orange       // 2
+)
+
+func main(){
+	// i := 1
+	// println("hello world".i)
+}
